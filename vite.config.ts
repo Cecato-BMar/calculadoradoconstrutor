81c2ba61063887b8b6106d3e85b72868612cb8a2
Cecato-BMar <129400192+Cecato-BMar@users.noreply.github.com>
2025-11-04 15:28:51 -0300

    Modify Vite config for build directory and extensions
    Updated build output directory from 'dist' to 'build' and added file extensions to resolve.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: 'dist', // Mude de 'build' para 'dist'
     target: 'esnext',
+    outDir: 'build',
   },
   server: {
     port: 3000,
     open: true,
   },
-})
+});
